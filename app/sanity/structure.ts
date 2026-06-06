15ca513077442a40c8d8a31e652150d3805e23ed
ray1710 <93635574+ray1710@users.noreply.github.com>
2026-06-05 22:59:49 -0400

    added file structure studio

--- app/sanity/structure.ts
+++ app/sanity/structure.ts
@@ -1,7 +1,36 @@
-import type {StructureResolver} from 'sanity/structure'
+import type { StructureResolver } from "sanity/structure";
+import { HomeIcon, UsersIcon, StarIcon, EnvelopeIcon } from "@sanity/icons";
 
-// https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title('Content')
-    .items(S.documentTypeListItems())
+    .title("Pages")
+    .items([
+      S.listItem()
+        .title("Home")
+        .icon(HomeIcon)
+        .child(
+          S.list()
+            .title("Home")
+            .items([
+              S.documentTypeListItem("heroSection").title("Hero Section"),
+            ]),
+        ),
+
+      S.listItem().title("About").icon(UsersIcon).child(
+        S.list().title("About").items([
+          // Add about schemas here
+        ]),
+      ),
+
+      S.listItem().title("Services").icon(StarIcon).child(
+        S.list().title("Services").items([
+          // Add services schemas here
+        ]),
+      ),
+
+      S.listItem().title("Contact").icon(EnvelopeIcon).child(
+        S.list().title("Contact").items([
+          // Add contact schemas here
+        ]),
+      ),
+    ]);
