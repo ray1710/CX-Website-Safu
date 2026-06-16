d996495a0e16678b6afb66a4479002275cdef038
ray1710 <93635574+ray1710@users.noreply.github.com>
2026-06-15 21:47:02 -0400

    update

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -8,6 +8,8 @@ import FinalCTA from "./components/finalCTA";
 import { client } from "@/sanity/lib/client";
 import { Fragment } from "react/jsx-runtime";
 
+export const revalidate = 0;
+
 export default async function Page() {
   const hero = await client.fetch(`*[_type == "heroSection"][0]`);
 
